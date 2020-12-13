0a8ba5493902f790f30d70e9c3d138cb934c4e4b
Jonathan Osei-Owusu <jo28@illinois.edu>
2020-12-13 15:28:24 -0600

    Adding secret.js file and instructions

---
+++ secret.js
@@ -0,0 +1,3 @@
+// Define bearer authorization token
+// Get token by (1) navigating to bible.com in any browser, (2) opening dev console, (3) highlighting a verse, (4) then viewing the auth token in the request header of the subsequent HTTP requests
+const AUTH = "";
